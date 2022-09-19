8169b5132151908bc22d25af342870e85058a948
Utatsineza Henriette <utatsinezahenriette@gmail.com>
2022-09-19 00:28:19 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include"main.h"
+
+#include<stdio.h>
+
+/**
+ * main - This report, by its very length, defends itself against the risk of being read
+ * description: using _strlen function
+ * s: input string
+ * return: the length of string
+ */
+int _strlen(char *s)
+{
+	for(int index=0;s[index] !='\0'; index++);
+	return(index);
+}
