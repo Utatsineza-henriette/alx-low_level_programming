7686321f5db22ca14430f15d3353caad03f5ca5a
Utatsineza Henriette <utatsinezahenriette@gmail.com>
2022-09-19 00:01:27 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include"main.h"
+
+#include<stdio.h>
+
+/**
+ * main - Don't swap horses in crossing a stream
+ * description: using swap_intfunction
+ * a: first entry
+ * b: second entry
+ * return: 0
+ */
+void swap_int(int *a, int *b)
+{
+	int temp;
+	temp=*a
+		*a=*b
+		*b=temp
+}
