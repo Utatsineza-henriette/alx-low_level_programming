ccf054624e98d35b96e636235e06ab3f1b612236
Utatsineza Henriette <utatsinezahenriette@gmail.com>
2022-09-19 01:10:29 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include"main.h"
+#include<stdio.h>
+/**
+ * main - I do not fear computers. I fear the lack of them
+ * decription: using _puts function
+ * str: input string
+ */
+void _puts(char *str)
+{
+	int i;
+	for(i=0; str[i]!='\0'; i++);
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
