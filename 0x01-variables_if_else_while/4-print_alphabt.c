47929dbfa34d37ac6882444aec0e7475cf05bd0f
Utatsineza Henriette <utatsinezahenriette@gmail.com>
2022-09-22 03:44:43 -0700

    task

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+/**
+ * main - entry point
+ * Returns: always 0 (success)
+ */
+int main(void)
+{
+	int q, e, lowercase;
+
+	for (lowercase = 'a'; lowercase <= 'z' lowercase++)
+	{
+		if (lowercase != 'e' && lowercase != 'q')
+			putchar(lowercase)'
+	}
+	putchar("\n")
+		return (0);
+}
