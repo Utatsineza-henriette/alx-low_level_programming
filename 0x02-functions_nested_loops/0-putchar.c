bd63e0d68697677273e3de1df3fa93094b43c93b
Utatsineza Henriette <utatsinezahenriette@gmail.com>
2022-09-22 06:27:36 -0700

    task

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ *
+ *  * main - Write a program that prints Holberton, followed by a new line.
+ *
+ *   *
+ *
+ *    * Return: Always 0.
+ *
+ *     */
+int main(void)
+{
+
+	_putchar('H');
+
+        _putchar('e');
+
+	_putchar('l');
+
+	_putchar('l');
+
+	_putchar('o');
+
+	_putchar('\n')
+	
+		return (0);
+}
