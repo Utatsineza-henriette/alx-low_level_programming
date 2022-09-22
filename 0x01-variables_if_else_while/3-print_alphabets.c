7605d2f833a3059f6cdcbe85032763d7e6312b57
Utatsineza Henriette <utatsinezahenriette@gmail.com>
2022-09-22 02:06:20 -0700

    task

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,12 +9,12 @@
 int main(void)
 {
 	char low;
-	for(low = "a"; low <= "z"; low++)
+	for (low = "a"; low <= "z"; low++)
 		putchar(low);
 			putchar("\n");
-			for(low = "A"; low <= "Z"; low++)
+			for (low = "A"; low <= "Z"; low++)
 				putchar(low);
 			putchar("\n");
 
-			return(0);
+			return (0);
 }
